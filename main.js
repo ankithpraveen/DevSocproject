fc75bbbe7b0124242b8f2c777c5e7cc099b00c66
Ankith Praveen <kith.304@gmail.com>
2021-03-26 23:19:22 +0530

    changed questions

--- main.js
+++ main.js
@@ -25,20 +25,25 @@ function totscore() {
 
     }
     var pc = (sessionStorage.getItem("score") * 100 / sessionStorage.getItem("totques"));
-    if (pc > 80) {
+    if (pc==100)
+    {
         var img = "https://i.imgflip.com/4/2x75fl.jpg";
     }
+    if (pc > 80) 
+    {
+        var img = "https://media.wired.com/photos/5e3246cd56bcac00087f0a1e/master/pass/Culture-Success-Meme-Kid.jpg";
+    }
     else if (pc > 60) {
-        var img = "https://i.ytimg.com/vi/fHS8tHKlgw4/mqdefault.jpg"
+        var img = "https://i.ytimg.com/vi/fHS8tHKlgw4/mqdefault.jpg";
     }
     else if (pc > 40) {
-        var img = "https://upload.wikimedia.org/wikipedia/en/1/1b/NPC_wojak_meme.png"
+        var img = "https://upload.wikimedia.org/wikipedia/en/1/1b/NPC_wojak_meme.png";
     }
     else if (pc > 30) {
-        var img = "https://i.kym-cdn.com/entries/icons/original/000/017/039/pressf.jpg"
+        var img = "https://i.kym-cdn.com/entries/icons/original/000/017/039/pressf.jpg";
     }
     else {
-        var img = "https://i.pinimg.com/originals/15/74/ac/1574ac1ef7a84a4bd69e83308a48e783.jpg"
+        var img = "https://i.pinimg.com/originals/15/74/ac/1574ac1ef7a84a4bd69e83308a48e783.jpg";
     }
     addhtml("finalres", "<i class=\"material-icons large cyan-text\">thumb_up</i><br><h4>Final Result</h4><h2> Your score is: " + (sessionStorage.getItem("score") * 100 / sessionStorage.getItem("totques")) + "%</h2><br><img src=\"" + img + "\"><\img>");
 }
