1d68c6e704fa7ef6b9d4911119804427a2340b77
Ankith Praveen <kith.304@gmail.com>
2021-03-26 21:51:18 +0530

    added answers on result page

--- main.js
+++ main.js
@@ -1,70 +1,68 @@
-var score=0;
-var index=0;
+var score = 0;
+var index = 0;
 
 
-function checkans(choice)
-{
+function checkans(choice) {
     console.log(questions[index][2]);
     console.log(choice);
-    if (questions[index][2]===choice)
-    {
+    if (questions[index][2] === choice) {
         score++;
     }
     index++;
-    console.log(index);
+    console.log(score);
 }
 
-function addhtml(id,html)
-{
-    var elem=document.getElementById(id);
-    elem.innerHTML=html;
+function addhtml(id, html) {
+    var elem = document.getElementById(id);
+    elem.innerHTML = html;
 }
 
-function totscore()
-{
+function totscore() {
     console.log(sessionStorage.getItem("score"));
     console.log(sessionStorage.getItem("totques"));
-    console.log(sessionStorage.getItem("ques"));
-    addhtml("finalres","<i class=\"material-icons large cyan-text\">thumb_up</i><br><h2> Your score is: " + (sessionStorage.getItem("score")*100/sessionStorage.getItem("totques")) + "%</h2>" + "<h4>Final Result</h4>");
+    var ansdiv =document.getElementById("answers");
+    for (var i = 0; i < sessionStorage.getItem("totques"); i++) {
+        ansdiv.innerHTML+="<h5>Q. "+sessionStorage.getItem("ques" + i)+"<br>A. "+sessionStorage.getItem("ans" + i);
+        console.log(sessionStorage.getItem("ques" + i));
+        console.log(sessionStorage.getItem("ans" + i));
+    }
+    addhtml("finalres", "<i class=\"material-icons large cyan-text\">thumb_up</i><br><h2> Your score is: " + (sessionStorage.getItem("score") * 100 / sessionStorage.getItem("totques")) + "%</h2>" + "<h4>Final Result</h4>");
 }
 
-function nextques()
-{
+function nextques() {
 
-    if (index>=questions.length)
-    {
+    if (index >= questions.length) {
         window.location.replace("result.html");
-        sessionStorage.setItem("score",score);
-        sessionStorage.setItem("totques",questions.length);
-        sessionStorage.setItem("ques",questions);
-    }
-    else
-    {
-        addhtml("question",questions[index][0])
-        for (var i=0; i<= questions[1].length; i++)
-        {
-            addhtml("choice"+i,questions[index][1][i]);
-            checkguess("guess"+i,questions[index][1][i])
+        sessionStorage.setItem("score", score);
+        sessionStorage.setItem("totques", questions.length);
+        for (var i = 0; i <= questions.length; i++) {
+            sessionStorage.setItem("ques" + i, questions[i][0]);
+            sessionStorage.setItem("ans"+i,questions[i][2]);
         }
-        
     }
-    
+    else {
+        addhtml("question", questions[index][0])
+        for (var i = 0; i <= questions[1].length; i++) {
+            addhtml("choice" + i, questions[index][1][i]);
+            checkguess("guess" + i, questions[index][1][i]);
+        }
+
+    }
+
 }
 
-function checkguess(id,guess)
-{
-    document.getElementById(id).onclick=function()
-    {
+function checkguess(id, guess) {
+    document.getElementById(id).onclick = function () {
         checkans(guess);
         nextques();
     }
 }
 
 var questions = [
-    ["Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson", "Thomas Edison", "I don't know" ], "George Washington"],
-    ["What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi","42", "Wah?", "I don't know"], "42"],
-    ["Do you love to code?", ["No","Yes", "Hell Yeah", "Maybe"], "Hell Yeah"],
-    ["What's the best programming language?", ["Javascript","C#", "Php", "Python"], "Javascript"],
+    ["Who was the first President of the United States?", ["George Washington", "Thomas Jefferson", "Thomas Edison", "I don't know"], "George Washington"],
+    ["What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi", "42", "Wah?", "I don't know"], "42"],
+    ["Do you love to code?", ["No", "Yes", "Hell Yeah", "Maybe"], "Hell Yeah"],
+    ["What's the best programming language?", ["Javascript", "C#", "Php", "Python"], "Javascript"],
 ];
 
 nextques();
